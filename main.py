e19a771863bf0b3ad313d6a3f3701278c51794e9
kotetsu599 <kotetsu499@gmail.com>
2024-08-09 10:34:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-ai = input("AIによるチャット規制を有効にしますか? y/n ※おすすめしません。荒らされると規制判定に時間がかかりすぎてスパム対策が動かなくなります。:")
+# これは荒らし対策ボットの一例です。これをそのまま荒らし対策に使うとまずいことになるかもしれませんので、計画的にご利用ください。
+# 初期設定: 5秒間に3回以上のメッセージでタイムアウト　メンション二回目でタイムアウト　
+
+
+ai = input("AIによるチャット規制を有効にしますか? y/n ※おすすめしません。荒らされると規制判定に時間がかかりすぎてスパム対策が動かなくなります。:") 
 if ai not in ["y","n"]:
     print("🤬🤬🤬🤬🤬")
     exit()
@@ -15,6 +19,7 @@ from datetime import timedelta
 from discord.utils import utcnow 
 import time
 import logging
+import asyncio
 file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bad_words.txt")  
 bad_words = []
 with open(file_path, 'r', encoding='utf-8') as f:
@@ -26,9 +31,10 @@ domains = [
     ".eu", ".tv", ".name", ".mobi", ".club", ".store", ".pro", ".co.uk", ".org.uk", ".me", ".ws",
     ".cc", ".tv", ".aero", ".museum", ".tel", ".int", ".jobs", ".travel", ".cat", ".coop", ".asia",
     ".tk", ".cf", ".gq", ".ml", ".vn", ".th", ".hk", ".sg", ".my", ".ph", ".id", ".cn", ".kr", ".tw",
-    ".jp", ".pk", ".bd", ".np", ".lk", ".in", ".ae", ".sa", ".qa", ".om", ".kw", ".bh", ".il", ".ir"
+    ".jp", ".pk", ".bd", ".np", ".lk", ".in", ".ae", ".sa", ".qa", ".om", ".kw", ".bh", ".il", ".ir","http"
 ]
 first_message = {}
+sent = []
 messages = []
 message_count = {}
 translations = {}
@@ -37,7 +43,7 @@ verified_that_bad = []
 GREEN = '\033[92m'
 YELLOW = "\033[33m"
 END = '\033[0m'
-mention = {}
+mention = []
 if ai == "y":
     warnings.filterwarnings("ignore", message="`clean_up_tokenization_spaces` was not set.")
     warnings.filterwarnings("ignore", message="Torch was not compiled with flash attention.")
@@ -62,16 +68,14 @@ client = discord.Client(intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
-    print("ボットが生きてる！")
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(client.guilds)} サーバー"))
+    print(f"{GREEN}ボットが起動しました。{END}")
 
 @client.event
 async def on_message(message):
     global first_message, messages, message_count
     global translations, verified_that_good, verified_that_bad
     messages.append(message)
-    
-    if message.author.name == "kotetsu_gen": #製作者には効かない
-        return
 
     if message.author.bot:
         return 
@@ -91,14 +95,13 @@ async def on_message(message):
         await message.delete()
         if message.author.name not in mention:
             await message.author.send(f"このサーバーではメンションが許可されていません。もう一度メンションをすると、タイムアウトになります。")
-        try:
-            mention[message.author.name] += 1
-        except:
-            mention[message.author.name] = 1
-        if mention[message.author.name]>=2:
+            mention.append(message.author.name)
+            return
+        else:
             timeout_duration = utcnow() + timedelta(seconds=60)
             await message.author.timeout(timeout_duration, reason="メンション")
             await message.author.send("あなたはメンションスパムによってタイムアウトされました。")
+            return
 
     if message.author.name not in message_count:
         message_count[message.author.name]=0
@@ -112,10 +115,14 @@ async def on_message(message):
         
     if message_count[message.author.name] >= 3:
         await message.delete()
+        if message.author.name in sent:
+            return
         timeout_duration = utcnow() + timedelta(seconds=30)
         await message.author.timeout(timeout_duration, reason="スパム")
         await message.author.send("あなたはスパムによってタイムアウトされました。")
+        sent.append(message.author.name)
         message_count[message.author.name] = 0
+        await asyncio.sleep(114514)
         return
 
     if ai == "y":
@@ -123,13 +130,12 @@ async def on_message(message):
         language = detect_language(message_content)
 
         if language == "ja":
-            print("detected that it is ja")
             if message.content not in translations:
                 for verified_that_goo in verified_that_good:
-                    if verified_that_goo in message.content:
+                    if verified_that_goo == message.content:
                         return
                 for verfied_that_ba in verified_that_bad:
-                    if verfied_that_ba in message.content:
+                    if verfied_that_ba == message.content:
                         await message.delete()
                         return
                     
@@ -141,7 +147,7 @@ async def on_message(message):
                 print(f"{GREEN}{translation}{END}")
                 print(f"{YELLOW}{score}{END}")
                 
-                if score >= 0 and score <= 0.1:
+                if score >= 0 and score <= 0.2:
                     verified_that_good.append(message.content)
                 else:
                     await message.delete()
@@ -150,11 +156,11 @@ async def on_message(message):
 
             else:
                 for verified_that_goo in verified_that_good:
-                    if verified_that_goo in message.content:
+                    if verified_that_goo == message.content:
                         return
                     
                 for verfied_that_ba in verified_that_bad:
-                    if verfied_that_ba in message.content:
+                    if verfied_that_ba == message.content:
                         await message.delete()
                         return
                     
@@ -165,37 +171,34 @@ async def on_message(message):
                 print(f"{GREEN}{translation}{END}")
                 print(f"{YELLOW}{score}{END}")
 
-                if score >= 0 and score <= 0.1:
+                if score >= 0 and score <= 0.2:
                     verified_that_good.append(message.content)
                 else:
                     await message.delete()
-
                     verified_that_bad.append(message.content)
 
 
         else:
             for verified_that_goo in verified_that_good:
-                if message.content in verified_that_good:
+                if message.content == verified_that_goo:
                     return
                 
             for verfied_that_ba in verified_that_bad:
-                if verfied_that_ba in message.content:
+                if verfied_that_ba == message.content:
                     await message.delete()
                     return
                 
-            translation = message_content
             classifier = pipeline("text-classification", model="unitary/toxic-bert", device=0 if torch.cuda.is_available() else -1)
-            results = classifier(translation)[0]
+            results = classifier(message.content)[0]
             score = results['score']
             
-            print(f"{GREEN}{translation}{END}")
+            print(f"{GREEN}{message_content}{END}")
             print(f"{YELLOW}{score}{END}")
 
-            if score >= 0 and score <= 0.1:
+            if score >= 0 and score <= 0.2:
                 verified_that_good.append(message.content)
             else:
                 verified_that_bad.append(message.content)
                 await message.delete()
        
-
-client.run("bot token")
+client.run("bot token here")
